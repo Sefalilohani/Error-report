bb04aa87e5412017f2060a5e94add4830c01998f
Sefalilohani <sefali.lohani@springworks.in>
2026-03-08 21:06:49 +0530

    Update report.py

--- report.py
+++ report.py
@@ -7,7 +7,7 @@ import urllib.parse
 
 # ── CONFIG ─────────────────────────────────────────────────────
 
-SLACK_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_TOKEN = os.environ["SLACK_BOT_TOKEN"].lower()
 
 REDASH_API_KEY = "sMdXlebHKozPGyJjOfAhRpH0S7ggmsSNE8GR5zc7"
 
